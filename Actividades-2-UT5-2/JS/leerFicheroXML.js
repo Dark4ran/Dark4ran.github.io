226bbd53855d5d1eca7efb3114213ec1bd1033dd
Jose Manuel <76516188+Dark4ran@users.noreply.github.com>
2021-05-20 17:08:05 +0200

    Add files via upload

--- Actividades-2-UT5-2/JS/leerFicheroXML.js
+++ Actividades-2-UT5-2/JS/leerFicheroXML.js
@@ -1,9 +1,14 @@
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let res = xmlDoc.querySelectorAll("respuesta")
+	let con = xmlDoc.querySelectorAll("mensaje")
 	for(let i=0; i<res.length; i++)
 	{
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + res[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='resp'>" + res[i].textContent + "</p>"
+	}
+	for(let i=0; i<con.length; i++)
+	{
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='cons'>" + con[i].textContent + "</p>"
 	}
 }
 
