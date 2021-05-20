042fa65af915ee472da6c4157c574e6947c49e70
Jose Manuel <76516188+Dark4ran@users.noreply.github.com>
2021-05-20 10:34:03 +0200

    Update leerFicheroXML.js

--- Actividades-2-UT5-2/JS/leerFicheroXML.js
+++ Actividades-2-UT5-2/JS/leerFicheroXML.js
@@ -4,11 +4,11 @@ function gestionarFicheroXML(xmlDoc){
 	let con = xmlDoc.querySelectorAll("mensaje")
 	for(let i=0; i<res.length; i++)
 	{
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class:"resp" >" + res[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class:";resp;" >" + res[i].textContent + "</p>"
 	}
 	for(let i=0; i<con.length; i++)
 	{
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class:"cons" >" + con[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class:";cons;" >" + con[i].textContent + "</p>"
 	}
 }
 
